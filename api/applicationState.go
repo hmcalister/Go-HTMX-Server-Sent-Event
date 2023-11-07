a3d2530fa73a0f6e0d7d57747247b158d6349a5c
Hayden McAlister <haydenmcalister49@gmail.com>
2023-11-08 00:24:33 +1300

    Update applicationState to handle clicks with mutex

--- api/applicationState.go
+++ api/applicationState.go
@@ -1,21 +1,30 @@
 package api
 
+import (
+	"log"
+	"sync"
+)
+
 type ApplicationState struct {
-	currentItemIndex int
+	totalClicks      int
+	totalClicksMutex sync.Mutex
 }
 
 func NewApplicationState() *ApplicationState {
-	return &ApplicationState{}
+	return &ApplicationState{
+		totalClicks: 0,
+	}
 }
 
-func (appState *ApplicationState) AddItem() int {
-	appState.currentItemIndex += 1
-	return appState.currentItemIndex
+func (appState *ApplicationState) AddClick() {
+	go func() {
+		appState.totalClicksMutex.Lock()
+		appState.totalClicks += 1
+		appState.totalClicksMutex.Unlock()
+		log.Printf("%v", appState.totalClicks)
+	}()
 }
 
-func (appState *ApplicationState) DeleteItem() {
-}
-
-func (appState *ApplicationState) DeleteAll() {
-	appState.currentItemIndex = 0
+func (appState *ApplicationState) GetClicks() int {
+	return appState.totalClicks
 }
